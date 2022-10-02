6a163b37821b4893a8cfa8d83ac0bbf6028f9299
matheusprv <matheusprv2003@gmail.com>
2022-10-02 15:27:28 -0300

    Inicio da parte de jogo

--- 2022-1/Programação/Trabalho_Pratico/TP-BCC201-Matheus_Peixoto/main.c
+++ 2022-1/Programação/Trabalho_Pratico/TP-BCC201-Matheus_Peixoto/main.c
@@ -19,6 +19,17 @@ void menuNovoJogo();
 
 void imprimeTabuleiro(char **);
 
+//Retorna 0 caso o jogo esteja acontecendo, 1 caso o jogador 1 ganhe, 2 caso o jogador 2 ganhe ou 3 caso de velha
+int jogoFinalziado(char **);
+
+int jogo(char **, char [][TAM_MAX_NOMES]);
+
+//1 para marcar, 2 para salvar, 3 para voltar, 0 caso inválido, -1 posicoes inválidas
+int lerComandos(int *, int*);
+
+
+
+
 int main(){
 
     char opcao;
@@ -89,7 +100,6 @@ void imprimeMenuPrincipal(int comandoValido){
 }
 
 
-
 void menuNovoJogo(){
     limparTerminal();
     //Verificando quantos jogadores farão parte da partida
@@ -114,6 +124,7 @@ void menuNovoJogo(){
         nomeJogadores[i][strlen(nomeJogadores[i]) -1] = '\0' ;
     }
 
+    //Criando a posição das jogadas
     char **disposicaoJogadas = malloc(3 * sizeof(char));
     for(int i=0; i<3;i++){
         disposicaoJogadas[i] = malloc (3 * sizeof(char));
@@ -124,7 +135,8 @@ void menuNovoJogo(){
         }
     }
 
-    imprimeTabuleiro(disposicaoJogadas);
+    int respostaJogo = jogo(disposicaoJogadas, nomeJogadores);
+
     liberarMatrizeChar(disposicaoJogadas);
 
     //Somente durante a produção, paa evitar que o jogo saia para o menu sem exibir o que foi modificado
@@ -132,7 +144,6 @@ void menuNovoJogo(){
     //scanf("%c", &lixo);
     getchar();
 }
-
 
 
 void imprimeTabuleiro(char **disposicaoJogadas){
@@ -155,4 +166,86 @@ void imprimeTabuleiro(char **disposicaoJogadas){
     }
     printf("\n");
 
+}
+
+int jogo(char **disposicaoJogadas, char nomeJogadores[][TAM_MAX_NOMES]){
+    
+    int numeroJogadas = -1;
+
+    do{
+        numeroJogadas++;
+        limparTerminal();
+        imprimeTabuleiro(disposicaoJogadas);
+        int linha, coluna, comando;
+
+        printf("%s, digite o comando: ", nomeJogadores[numeroJogadas%2]);
+        comando = lerComandos(&linha, &coluna);
+
+        while(comando == 0 || comando == -1){
+            printf("Comando inválido\n");
+            printf("%s, digite o comando: ", nomeJogadores[numeroJogadas%2]);
+            comando = lerComandos(&linha, &coluna);
+        }
+
+        switch(comando){
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+        }
+
+    }while(!jogoFinalziado(disposicaoJogadas));
+
+    return 1;
+}
+
+//Retorna 0 caso o jogo esteja acontecendo, 1 caso o jogador 1 ganhe, 2 caso o jogador 2 ganhe ou 3 caso de velha
+int jogoFinalziado(char **disposicaoJogadas){
+    return 1;
+}
+
+//1 para marcar, 2 para salvar, 3 para voltar, 0 caso inválido, -1 posicoes inválidas
+int lerComandos(int *linha, int *coluna){
+    
+    char comando[12];
+    //Lendo o comando feito pelo usuário
+    fgets(comando, 12, stdin);
+    comando[strlen(comando)-1] = '\0';
+    int tamanhoComando = strlen(comando);
+
+    //Verificando se foi digitado um comando válido
+    if (strstr(comando, "marcar") != NULL) {
+        
+        char charLinha = comando[tamanhoComando-2];
+        char charColuna = comando[tamanhoComando-1];
+
+        //Transforma o char em inteiro a partir da subtração do valor do inteiro na tabela ascii com o valor de zero também na tabela ascii
+        *linha = charLinha - '0';
+        *coluna = charColuna - '0';
+
+        if(*linha<1 || *linha>3 || *coluna<1 || *coluna>3){
+            if(*linha<1 || *linha>3){
+                printf("Linha inválida.\n");
+            }
+            if(*coluna<1 || *coluna>3){
+                printf("Coluna inválida.\n");
+            }
+            return -1;
+        }
+
+        return 1;
+    }
+    else if (strstr(comando, "salvar") != NULL) {
+        return 2;
+    }
+    else if (strstr(comando, "voltar") != NULL) {
+        return 3;
+    }
+    else{
+        return 0;
+    }
 }
