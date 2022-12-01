e37b6229f8b48a6f1aff3cb4d9bd6864455ecc2a
matheusprv <matheusprv2003@gmail.com>
2022-12-01 18:14:58 -0300

    Pratica 1 de ED1

--- 2022-2/Estrutura_de_dados_1/Pratica_1/matriz.c
+++ 2022-2/Estrutura_de_dados_1/Pratica_1/matriz.c
@@ -40,29 +40,42 @@ void lerMatriz(double **M, int ordem) {
 }
 
 double somaMatriz(double **M, int ordem) {
+    // double soma = 0;
+    // int linhaParada = ordem/2;
+    // for(int i = 0; i < linhaParada-1; i++){
+    //     for(int j=i+1; j<ordem-(i+1); j++){
+    //         soma += M[i][j];
+    //     }
+    // }
+    // return soma;
     double soma = 0;
-    int linhaParada = ordem%2 == 0 ? ordem/2 : (ordem/2) + 1 ;
-    //int linhaParada = ordem/2;
-    for(int i = 0; i < linhaParada-1; i++){
-        for(int j=i+1; j<ordem-(i+1); j++){
-            soma += M[i][j];
+    int inicio = 0;
+    int final = ordem-1;
+
+    for(int linha = 0; (inicio != final) && (inicio+1 != final); linha++){
+        for(int coluna = linha + 1; coluna < ordem - (linha+1); coluna++){
+            soma+= M[linha][coluna];
         }
+        inicio++;
+        final--;
     }
     return soma;
 
 }
 
 double media(double resultado, int ordem) {
+
     int qtdItensSomados = 0;
-        
-    int linhaParada = ordem%2 == 0 ? ordem/2 : (ordem/2) + 1 ;
-    //int linhaParada = ordem/2;
-    for(int i = 0; i < linhaParada-1; i++){
-        for(int j=i; j<ordem-i; j++){
+    int inicio = 0;
+    int final = ordem-1;
+
+    for(int linha = 0; (inicio != final) && (inicio+1 != final); linha++){
+        for(int coluna = linha + 1; coluna < ordem - (linha+1); coluna++){
             qtdItensSomados++;
         }
+        inicio++;
+        final--;
     }
-
     return resultado/qtdItensSomados;
 }
 
