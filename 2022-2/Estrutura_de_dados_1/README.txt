0d2908dd7e1c9129ee848c84ac346992eca6a6c6
matheusprv <matheusprv2003@gmail.com>
2022-12-01 17:38:55 -0300

    Semestre 2022.1

---
+++ 2022-2/Estrutura_de_dados_1/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 01/12/2022 17:36:38 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Prática 1
---
+++ 2022-2/Estrutura_de_dados_1/matriz.c
@@ -0,0 +1,71 @@
+#include "matriz.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+void lerOrdem(int *ordem) {
+    scanf("%d", ordem);
+}
+
+//manter como especificado
+double **alocaMatriz(int ordem) {
+
+    double **matriz = malloc(ordem * sizeof(double*)) ;
+    for(int i=0; i<ordem; i++){
+        matriz[i] = malloc(ordem * sizeof(double));
+    }
+    return matriz;
+
+}
+
+//manter como especificado
+void desalocaMatriz(double ***M, int ordem) {
+    for(int i=0; i < ordem; i++){
+        free((*M)[i]);
+    }
+    free(*M);
+}
+
+//manter como especificado
+void lerOperacao(char *operacao) {
+    getchar();
+    scanf("%c", operacao);
+}
+
+void lerMatriz(double **M, int ordem) {
+    for(int i=0; i<ordem; i++){
+        for(int j=0; j<ordem; j++){
+            scanf("%lf", &M[i][j]);
+        }
+    }
+}
+
+double somaMatriz(double **M, int ordem) {
+    double soma = 0;
+    int linhaParada = ordem%2 == 0 ? ordem/2 : (ordem/2) + 1 ;
+    //int linhaParada = ordem/2;
+    for(int i = 0; i < linhaParada-1; i++){
+        for(int j=i+1; j<ordem-(i+1); j++){
+            soma += M[i][j];
+        }
+    }
+    return soma;
+
+}
+
+double media(double resultado, int ordem) {
+    int qtdItensSomados = 0;
+        
+    int linhaParada = ordem%2 == 0 ? ordem/2 : (ordem/2) + 1 ;
+    //int linhaParada = ordem/2;
+    for(int i = 0; i < linhaParada-1; i++){
+        for(int j=i; j<ordem-i; j++){
+            qtdItensSomados++;
+        }
+    }
+
+    return resultado/qtdItensSomados;
+}
+
+void printResultado(double resultado) {
+    printf("%.1lf\n", resultado);
+}
