d2c609dabbca03e61ed0496e224c3bd8fade8615
matheusprv <matheuspeixotoribeiro@hotmail.com>
2023-01-31 23:09:11 -0300

    Tutoria Q1 finalizada

--- 2022-2/Estrutura_de_dados_1/Tutoria_1/Exec_1/main.c
+++ 2022-2/Estrutura_de_dados_1/Tutoria_1/Exec_1/main.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
 #include "lista.h"
 
+#define MAX_TAM 100000
+
 int main(){
     
     TLista lista;
     TLista_Inicia(&lista);
 
-    char string[100000];
+    char string[MAX_TAM];
     int tamString;
 
-    char caracter;
-    //HOME = -1  --  END = 1
-    int homeEnd = 1;
+    char temp[MAX_TAM];
+    int tamTemp = -1;
 
-    while(scanf("%s", string) != EOF){
+    char caracter;
+
+
+    while(fgets(string, MAX_TAM, stdin) != NULL){
 
         tamString = strlen(string);
+        if(string[tamString-1] == '\n'){
+            string[tamString-1] = '\0';
+            tamString--;
+        }
 
-        for(int i = 0; i< tamString; i++){
 
+        for(int i = string[0] == '[' ? 1 : 0; i < tamString; i++){
             caracter = string[i];
 
-            if(caracter == '[' || caracter == ']'){
-                homeEnd *= -1;
-                continue;
+            if(caracter == '['){
+                
+                int j;
+                for(j=i+1; j<tamString && string[j] != '[' && string[j] != ']'; j++){
+                    tamTemp++;
+                    temp[tamTemp] = string[j];
+                }
+                for(int k = tamTemp; k>=0; k--){
+                    TLista_Insere_Inicio(&lista, temp[k]);
+                }
+                temp[tamTemp+1]='\0';
+                tamTemp = -1;
+                i = j-1;
             }
-
-            if(homeEnd == 1){
+            else if (caracter != ']'){
                 TLista_Insere_Fim(&lista, caracter);
             }
-            else{
-                TLista_Insere_Inicio(&lista, caracter);
-            }
+
         }
 
         TLista_Imprime(&lista);
