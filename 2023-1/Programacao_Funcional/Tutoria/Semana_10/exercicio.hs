45fa570ce8e950db350202cc057f10b33158c524
matheusprv <matheusprv2003@gmail.com>
2023-07-23 20:13:30 -0300

    Tutoria semana 10 finalizada

--- 2023-1/Programacao_Funcional/Tutoria/Semana_10/exercicio.hs
+++ 2023-1/Programacao_Funcional/Tutoria/Semana_10/exercicio.hs
@@ -38,7 +38,7 @@ data UDP = UDP {
     plength :: Field,
     checksum :: Field,
     pdata :: [Field]
-} deriving (Eq, Ord)
+} deriving (Eq, Ord, Show)
 
 
 --Implemente um parser para o tipo |Bit|, de forma que o dígito 1 seja representado pelo construtor |I| e o dígito 0 por |O|.
@@ -66,8 +66,6 @@ bitList n = (:) <$> bitParser <*> bitList (n-1)
     [([I,I,O],"011")]
 -}
 
-
-
 -- Usando parser |bitList| implemente um parser para o tipo |Field|
 -- Note que |fieldParser n| retorna um valor de tipo |Field| contendo uma 
 -- lista de |n| bits e o inteiro n como campos deste registro.
@@ -76,17 +74,44 @@ fieldParser n = Parser (\ inp -> case inp of
         [] -> []
         xs -> [extract n $ runParser (bitList n) xs]
     )
-
-extract :: Int ->  [([Bit], a)] -> (Field, a)
-extract n [(value, rest)] = (Field n value, rest)
+    where
+        extract n [] = (Field n [], [])
+        extract n [(value, rest)] = (Field n value, rest)
 {-
     runParser (fieldParser 5) "110011"
     [(Field {size = 5, content = [I,I,O,O,I]},"1")]
 -}
 
-
-
 -- Finalmente, usando os tamanhos de sequências de bits de cada campo de um datagrama UDP, 
 -- construa um parser para o tipo |UDP|.
-udpParser :: Parser Char UDP
-udpParser = undefined
+-- udpParser :: Parser Char UDP
+-- udpParser = Parser (\inp -> case inp of
+--         [] -> []
+--         xs -> [(
+--             UDP <$> fieldParser 16 <*> fieldParser 16 <*> fieldParser 16 <*> fieldParser 16 <*> many (fieldParser 32),
+--             drop 96 xs)]
+--     )
+
+udpParser = UDP <$> fieldParser 16 <*>
+                      fieldParser 16 <*>
+                      fieldParser 16 <*>
+                      fieldParser 16 <*>
+                      many (fieldParser 32)
+{-
+runParser udpParser "111111111100000000001111111111000000000011111111110000000000111111111100000000001111111111000000"
+[(
+    UDP {
+        source = Field {size = 16, content = [I,I,I,I,I,I,I,I,I,I,O,O,O,O,O,O]}, 
+        destination = Field {size = 16, content = [O,O,O,O,I,I,I,I,I,I,I,I,I,I,O,O]}, 
+        plength = Field {size = 16, content = [O,O,O,O,O,O,O,O,I,I,I,I,I,I,I,I]}, 
+        checksum = Field {size = 16, content = [I,I,O,O,O,O,O,O,O,O,O,O,I,I,I,I]}, 
+        pdata = [Field {size = 32, content = [I,I,I,I,I,I,O,O,O,O,O,O,O,O,O,O,I,I,I,I,I,I,I,I,I,I,O,O,O,O,O,O]}]},""),
+        
+        (UDP {
+            source = Field {size = 16, content = [I,I,I,I,I,I,I,I,I,I,O,O,O,O,O,O]}, 
+            destination = Field {size = 16, content = [O,O,O,O,I,I,I,I,I,I,I,I,I,I,O,O]}, 
+            plength = Field {size = 16, content = [O,O,O,O,O,O,O,O,I,I,I,I,I,I,I,I]}, 
+            checksum = Field {size = 16, content = [I,I,O,O,O,O,O,O,O,O,O,O,I,I,I,I]}, 
+            pdata = []},"11111100000000001111111111000000"
+        )]
+-}
